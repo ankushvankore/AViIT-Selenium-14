88d37ab843e0986038034562e2db16cc251010af
Ankush <ankushvankore@gmail.com>
2025-11-24 22:05:53 +0530

    Date: 24 Nov 2025

---
+++ MyAutomationProject/src/com/TestNGDemos/D01FirstTest.java
@@ -0,0 +1,26 @@
+package com.TestNGDemos;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class D01FirstTest {
+	@Test
+	public void myFirstTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.google.com");
+		System.out.println("Title: " + driver.getTitle());
+		
+		driver.close();
+	}
+	
+	//@Test
+	public void trial() {
+		System.out.println("This is trail method");
+	}
+}
