9d12a9e015acd5ddb43419bfa90a1cb79918cf8d
Ankush <ankushvankore@gmail.com>
2025-11-25 21:30:55 +0530

    Date: 25 Nov 2025

---
+++ MyAutomationProject/src/com/Assignments/LoginToOHRM.java
@@ -0,0 +1,64 @@
+package com.Assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginToOHRM {
+	WebDriver driver;
+
+	@Test
+	public void login1() {
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+	@Test
+	public void login2() {
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+	@Test
+	public void login3() {
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("trupti");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("trupti23");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+	@Test
+	public void login4() {
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("mehwash");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("mehwash23");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+	@AfterMethod
+	public void logout() {
+		if (driver.getCurrentUrl().contains("dashboard")) {
+			driver.findElement(By.xpath("//i[@class='oxd-icon bi-caret-down-fill oxd-userdropdown-icon']")).click();
+			driver.findElement(By.partialLinkText("Log")).click();
+		}
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.close();
+	}
+
+}
--- MyAutomationProject/src/com/TestNGDemos/D04Before_AfterMethod.java
+++ MyAutomationProject/src/com/TestNGDemos/D04Before_AfterMethod.java
@@ -4,12 +4,14 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class D03Before_AfterTest {
-	WebDriver driver;
+public class D04Before_AfterMethod {
+WebDriver driver;
 	
 	@AfterTest
 	public void closeBrowser() {
@@ -23,27 +25,32 @@ public class D03Before_AfterTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void printTitle() {
+		System.out.println("Title: " + driver.getTitle());
+	}
+	
 	@Test (priority = 2)
 	public void testFirstCry() {
 		driver.get("https://www.firstcry.com/");
-		System.out.println("Title: " + driver.getTitle());
 	}
 	
 	@Test (priority = 4)
 	public void testBigBasket() {
 		driver.get("https://www.bigbasket.com/");
-		System.out.println("Title: " + driver.getTitle());
 	}
 	
 	@Test (priority = 3)
 	public void testAmazon() {
 		driver.get("https://www.amazon.in/");
-		System.out.println("Title: " + driver.getTitle());
 	}
 	
 	@Test (priority = 1)
 	public void testNykaa() {
 		driver.get("https://www.nykaa.com/");
-		System.out.println("Title: " + driver.getTitle());
 	}
 }
