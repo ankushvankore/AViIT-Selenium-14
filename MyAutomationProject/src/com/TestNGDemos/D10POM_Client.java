3c523a49bdb0a6fa6529096f8b3623f3065cc02b
Ankush <ankushvankore@gmail.com>
2025-12-08 19:45:34 +0530

    Date: 08 Dec 2025

--- MyAutomationProject/src/com/TestNGDemos/D10POM_Client.java
+++ MyAutomationProject/src/com/TestNGDemos/D10POM_Client.java
@@ -1,16 +1,16 @@
 package com.TestNGDemos;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
-
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class D10POM_Client {
-	public static WebDriver driverClient;
+	WebDriver driver;
 	RediffUtility r1;
 	@Test
 	public void createUser() {
@@ -19,16 +19,30 @@ public class D10POM_Client {
 		r1.setPassword("Amita@123");
 		r1.setBirthDate("01", "APR", "2000");
 	}
-
+	
+	@Test
+	public void createUser2() {
+		r1.setFullName("Trupti Killedar");
+		r1.setRediffId("truptikilledar");
+		r1.setPassword("Trupti!23");
+		r1.setBirthDate("31", "JAN", "2002");
+	}
+	
+	@BeforeMethod
+	public void refreshPage() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+	}
+	
 	@BeforeTest
 	public void beforeTest() {
-		driverClient = new EdgeDriver();
-		driverClient.manage().window().maximize();
-		driverClient.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		driverClient.get("https://register.rediff.com/register/register.php?FormName=user_details");
+		driver.get("https://register.rediff.com/register/register.php?FormName=user_details");
 		
-		r1 = new RediffUtility();
+		r1 = new RediffUtility(driver);
 	}
 
 	@AfterTest
--- MyAutomationProject/src/com/TestNGDemos/RediffUtility.java
+++ MyAutomationProject/src/com/TestNGDemos/RediffUtility.java
@@ -7,19 +7,23 @@ import org.openqa.selenium.support.ui.Select;
 public class RediffUtility {
 	WebDriver driver;
 	
+	public RediffUtility(WebDriver d) {
+		driver = d;
+	}
+	
 	public void setFullName(String fn) {
-		D10POM_Client.driverClient.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
 	}
 	public void setRediffId(String rid) {
-		D10POM_Client.driverClient.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[3]/div[1]/input[1]")).sendKeys(rid);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[3]/div[1]/input[1]")).sendKeys(rid);
 	}
 	public void setPassword(String ps) {
-		D10POM_Client.driverClient.findElement(By.xpath("//input[@id='newpasswd']")).sendKeys(ps);
-		D10POM_Client.driverClient.findElement(By.xpath("//input[@id='newpasswd1']")).sendKeys(ps);
+		driver.findElement(By.xpath("//input[@id='newpasswd']")).sendKeys(ps);
+		driver.findElement(By.xpath("//input[@id='newpasswd1']")).sendKeys(ps);
 	}
 	public void setBirthDate(String day, String month, String year) {
-		new Select(D10POM_Client.driverClient.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[1]"))).selectByVisibleText(day);
-		new Select(D10POM_Client.driverClient.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[2]"))).selectByVisibleText(month);
-		new Select(D10POM_Client.driverClient.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[3]"))).selectByVisibleText(year);
+		new Select(driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[1]"))).selectByVisibleText(day);
+		new Select(driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[2]"))).selectByVisibleText(month);
+		new Select(driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[3]"))).selectByVisibleText(year);
 	}
 }
