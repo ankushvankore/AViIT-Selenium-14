df551cb9fc60df24c20b060ec2188818ce69864f
Ankush <ankushvankore@gmail.com>
2025-12-12 19:56:23 +0530

    Date: 12 Dec 2025

--- MyAutomationProject/src/com/TestNGDemos/D13CreateLoginData.java
+++ MyAutomationProject/src/com/TestNGDemos/D13CreateLoginData.java
@@ -1,7 +1,9 @@
 package com.TestNGDemos;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -20,34 +22,58 @@ public class D13CreateLoginData {
 	XSSFSheet sheet;
 	XSSFRow row;
 	XSSFCell cell;
+	int index = 0;
 	
 	@Test(dataProvider = "getLoginData")
-	public void f(String un, String ps, String rs) {
-		System.out.println(un);
-		System.out.println(ps);
-		System.out.println(rs);
+	public void f(String un, String ps) {
+		/*row = sheet.createRow(index);
+		cell = row.createCell(0);
+		cell.setCellValue(un);
+		
+		cell = row.createCell(1);
+		cell.setCellValue(ps);
+		
+		cell = row.createCell(2);
+		cell.setCellValue("Not Run");*/
+		
+		sheet.createRow(index).createCell(0).setCellValue(un);
+		sheet.getRow(index).createCell(1).setCellValue(ps);
+		sheet.getRow(index).createCell(2).setCellValue("Not Run");
+		
+		index++;
 	}
 
 	@DataProvider
 	public Object[][] getLoginData() {
 		return new Object[][] {
-			new Object[] { "User Name", "Password", "Result" },
-			new Object[] { "admin", "admin123", "Not Run" },
-			new Object[] { "amita", "amita123", "Not Run" },
-			new Object[] { "admin", "admin123", "Not Run" },
-			new Object[] { "trupti", "trupti123", "Not Run" },
+			new Object[] { "admin", "admin123" },
+			new Object[] { "amita", "amita123" },
+			new Object[] { "admin", "admin123" },
+			new Object[] { "trupti", "trupti123" },
 		};
 	}
 	@BeforeTest
-	public void beforeTest() {
-		/*file = new File(fPath);
+	public void beforeTest() throws FileNotFoundException {
+		file = new File(fPath);
 		fos = new FileOutputStream(file);
 		wb = new XSSFWorkbook();
-		sheet = wb.createSheet("Login Data");*/
+		sheet = wb.createSheet("Login Data");
+		
+		sheet.createRow(in
+				dex).createCell(0).setCellValue("User Name");
+		sheet.getRow(index).createCell(1).setCellValue("Password");
+		sheet.getRow(index).createCell(2).setCellValue("Result");
+		
+		index++;
 	}
 
 	@AfterTest
-	public void afterTest() {
+	public void afterTest() throws IOException {
+		wb.write(fos);
+		wb.close();
+		fos.close();
+		
+		System.out.println("Data added succesfully!!!");
 	}
 
 }
