37ab6d1f2fa17f0cd6e95f9fcb5b4d2ddb2fb74f
Ankush <ankushvankore@gmail.com>
2025-12-17 20:22:54 +0530

    Date: 17 Dec 2025

--- MyAutomationProject/src/com/TestNGDemos/D15LoginToOHRM_DDF.java
+++ MyAutomationProject/src/com/TestNGDemos/D15LoginToOHRM_DDF.java
@@ -6,57 +6,99 @@ import org.testng.annotations.BeforeTest;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
 public class D15LoginToOHRM_DDF {
 	String fPath = "ExcelFiles\\OrangeORMLoginData.xlsx";
 	File file;
 	FileInputStream fis;
+	FileOutputStream fos;
 	XSSFWorkbook wb;
 	XSSFSheet sheet;
 	XSSFRow row;
 	XSSFCell cell;
+	WebDriver driver;
+	int index = 1;
+	String message;
 	
-	@Test(dataProvider = "dp")
-	public void f(String un, String ps, String rs, String ms) {
-		System.out.println(un);
-		System.out.println(ps);
-		System.out.println(rs);
-		System.out.println(ms);
-		System.out.println("----------------------------");
+	@Test(dataProvider = "getLoginData")
+	public void loginToOHRM(String un, String ps) {
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys(un);
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(ps);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 
 	@DataProvider
-	public Object[][] dp() {
-		String[][]loginData = new String[7][4];
+	public Object[][] getLoginData() {
+		int rows = sheet.getPhysicalNumberOfRows();
+		String[][]loginData = new String[rows-1][2];
 		
-		for(int i = 0; i < 7; i++) {
+		for(int i = 1; i < rows; i++) {
 			row = sheet.getRow(i);
-			for(int j = 0; j < 4; j++) {
+			for(int j = 0; j < 2; j++) {
 				cell = row.getCell(j);
-				loginData[i][j] = cell.getStringCellValue();
+				loginData[i-1][j] = cell.getStringCellValue();
 			}
 		}
 		return loginData;
 	}
+	
+	@AfterMethod
+	public void logout() {
+		row = sheet.getRow(index);
+		cell = row.getCell(2);
+		
+		if (driver.getCurrentUrl().contains("dashboard")) {
+			message = driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name']")).getText();
+			driver.findElement(By.xpath("//i[@class='oxd-icon bi-caret-down-fill oxd-userdropdown-icon']")).click();
+			driver.findElement(By.partialLinkText("Log")).click();
+			cell.setCellValue("Pass");
+		}
+		else {
+			cell.setCellValue("Fail");
+			message = driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p oxd-alert-content-text']")).getText();
+		}
+		
+		sheet.getRow(index).getCell(3).setCellValue(message);
+		index++;
+	}
+	
 	@BeforeTest
 	public void beforeTest() throws IOException {
 		file = new File(fPath);
 		fis = new FileInputStream(file);
 		wb = new XSSFWorkbook(fis);
 		sheet = wb.getSheetAt(0);	//Index starts with 0
+		fos = new FileOutputStream(file);
+		//As already the file is busy with fis object to read the workbook
+		//so always configure fos object after reading the sheet
+		
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	}
 
 	@AfterTest
 	public void afterTest() throws IOException {
+		wb.write(fos);
 		wb.close();
 		fis.close();
+		
+		driver.close();
 	}
 
 }
